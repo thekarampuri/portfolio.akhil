4694521703320893dd098da16ec7d10421d09d9b
thekarampuri <akhilkarampuri25@gmail.com>
2026-02-08 20:09:50 +0530

    chore: initial setup and configuration files

---
+++ next.config.ts
@@ -0,0 +1,34 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imagekit.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.dribbble.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images-na.ssl-images-amazon.com',
+      },
+    ],
+  },
+  eslint: {
+    // ✅ This makes Vercel ignore ESLint errors during builds
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
